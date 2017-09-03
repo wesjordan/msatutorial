d93bf34a2a30b2e17691e7798b8230e238d2e781
wesjordan <jordan.wesley@gmail.com>
2017-09-03 15:45:59 +0200

    jpa support added for teams (#2)

--- src/main/java/com/wesjordan/msatutorial/MsatutorialApplication.java
+++ src/main/java/com/wesjordan/msatutorial/MsatutorialApplication.java
@@ -1,12 +1,39 @@
 package com.wesjordan.msatutorial;
 
+import com.wesjordan.msatutorial.domain.Team;
+import com.wesjordan.msatutorial.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MsatutorialApplication {
 
+	@Autowired
+	TeamRepository teamRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MsatutorialApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		List<Team> teams = new ArrayList<>();
+
+		Team team = new Team();
+		team.setLocation("Harlem");
+		team.setName("Globetrotters");
+		teams.add(team);
+
+		team = new Team();
+		team.setLocation("Washington");
+		team.setName("Generals");
+		teams.add(team);
+
+		teamRepository.save(teams);
 	}
 }
