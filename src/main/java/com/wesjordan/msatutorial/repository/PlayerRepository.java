30f45ce98a6ec21cdf8f6bc7371468fd7d1c3d41
wesjordan <jordan.wesley@gmail.com>
2017-09-03 16:35:43 +0200

    repositories converted to rest resources (#5)

--- src/main/java/com/wesjordan/msatutorial/repository/PlayerRepository.java
+++ src/main/java/com/wesjordan/msatutorial/repository/PlayerRepository.java
@@ -1,10 +1,9 @@
 package com.wesjordan.msatutorial.repository;
 
-import com.wesjordan.msatutorial.domain.Team;
+import com.wesjordan.msatutorial.domain.Player;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(path="teams", rel="team")
-public interface TeamRepository extends CrudRepository<Team, Long>{
-
+@RestResource(path="players", rel="player")
+public interface PlayerRepository extends CrudRepository<Player, Long>{
 }
